8bbd9788dd0194697e188f7a2ff6e7ea31f9fd3c
Dr Nic Williams <drnicwilliams@gmail.com>
2012-07-14 08:54:46 -0700

    Allow common/utils.sh to be loaded into shell
    If loaded within monit ctl scripts then pipe output
    If loaded from 'source ../utils.sh' then normal STDOUT

--- src/common/utils.sh
+++ src/common/utils.sh
@@ -1,9 +1,14 @@
 
-SCRIPT=$(basename $0)
-mkdir -p /var/vcap/sys/log/monit
+# If loaded within monit ctl scripts then pipe output
+# If loaded from 'source ../utils.sh' then normal STDOUT
+if [[ $0 -ne '-bash' ]]
+then
+  SCRIPT=$(basename $0)
 
-exec 1>> /var/vcap/sys/log/monit/$SCRIPT.log
-exec 2>> /var/vcap/sys/log/monit/$SCRIPT.err.log
+  exec 1>> /var/vcap/sys/log/monit/$SCRIPT.log
+  exec 2>> /var/vcap/sys/log/monit/$SCRIPT.err.log
+fi
+mkdir -p /var/vcap/sys/log/monit
 
 pid_guard() {
   pidfile=$1
