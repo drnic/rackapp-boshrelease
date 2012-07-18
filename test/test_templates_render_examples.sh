a9caede1ba6aafdc135eaa63dc6a722575cff760
Dr Nic Williams <drnicwilliams@gmail.com>
2012-07-18 09:23:41 -0700

    WIP test for testing all examples of properties against job templates

---
+++ test/test_templates_render_examples.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env roundup
+
+describe "job templates render all examples"
+
+set -e # exit immediately if a simple command exits with a non-zero status
+set -u # report the usage of uninitialized variables
+
+[ "$(whoami)" != 'root' ] && ( echo ERROR: run as root user; exit 1 )
+
+cd /vagrant/ # need to hardcode as roundup overrides $0
+release_path=$(pwd)
+scripts=${release_path}/scripts
+
+examples=$(ls -R ${release_path}/examples/*.yml)
+
+it_configures_successfully_for_each_example() {
+  echo "|"
+  echo "| Stopping any existing jobs"
+  echo "|"
+  ${scripts}/stop
+
+  for example in ${examples}
+  do
+    echo "|"
+    echo "| Testing configuration template rendering:"
+    echo "| * ${example}"
+    echo "|"
+    result=$(${scripts}/configure ${example})
+    test ${result} =~ 'Applying job postgres using release'
+    test ${result} =~ 'Applying job webapp using release'
+    # and implicitly don't exit early due to template/properties mismatches
+  done
+}
